f1f62a15dc6cc52c7b299aadb082c26df6fc2135
mr.ahmedebrahim@hotmail.com <mr.ahmedebrahim@hotmail.com>
2019-03-31 15:45:42 +0200

    homescreen

--- src/modules/components/home.js
+++ src/modules/components/home.js
@@ -6,15 +6,59 @@ import {
   Footer,
   FooterTab,
   Button,
+  Input,
   Icon,
-  Text
+  Text,
+  ListItem,
+  List,
+  Thumbnail,
+  Left,
+  Right,
+  Item,
+  Body
 } from "native-base";
+import images from "../../../assets/images";
+
 export default class HomeScreen extends Component {
+  _onChangeSearchText(text) {
+    //do something
+  }
   render() {
     return (
       <Container>
-        <Header />
-        <Content />
+        <Header searchBar rounded autoCorrect={false}>
+          <Item>
+            <Icon name="ios-search" />
+            <Input
+              onChangeText={this._onChangeSearchText.bind(this)} // <-- Here
+              placeholder="Search"
+            />
+            <Icon name="ios-people" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={images.logo} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={1}>
+                  Its time to build a difference . .
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
         <Footer>
           <FooterTab>
             <Button vertical>
